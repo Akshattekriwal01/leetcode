7f8f5774679ed8c061fcefb46b19dd0ee62f6bad
peng-wang-guavus <wangpeng4515@gmail.com>
2020-04-02 22:37:12 +0800

    LC778 Iterative DFS & Binary Search

---
+++ Leetcode 778. Swim in Rising Water.py
@@ -0,0 +1,50 @@
+"""
+778. Swim in Rising Water
+
+On an N x N grid, each square grid[i][j] represents the elevation at that point (i,j).
+
+Now rain starts to fall. At time t, the depth of the water everywhere is t. You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distance in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+
+You start at the top left square (0, 0). What is the least time until you can reach the bottom right square (N-1, N-1)?
+
+Example 1:
+
+Input: [[0,2],[1,3]]
+Output: 3
+Explanation:
+At time 0, you are in grid location (0, 0).
+You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
+
+You cannot reach point (1, 1) until time 3.
+When the depth of water is 3, we can swim anywhere inside the grid.
+"""
+
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+        def canSwim(T):
+            # iterative dfs is faster than recursive
+            # time: O(N^2)
+            # space: O(N^2)
+            stack = [(0, 0)]
+            visited = set([(0, 0)])
+            while stack:
+                i, j = stack.pop()
+                if i == N - 1 and j == N - 1:
+                    return True
+                for di, dj in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                    r, c = i + di, j + dj
+                    if 0 <= r < N and 0 <= c < N and grid[r][c] <= T and (r, c) not in visited:
+                        visited.add((r, c))
+                        stack.append((r, c))
+            return False
+
+        lo, hi = grid[0][0], N * N
+        while lo < hi:
+            # time: O(logN)
+            mid = lo + (hi - lo) // 2
+            if canSwim(mid):
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
