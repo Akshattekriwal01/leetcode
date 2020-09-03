5479655f32bd7ad761d025a67d98971b25aeb556
Peng Wang <wangpeng4515@gmail.com>
2020-09-02 21:16:21 -0700

    use queue for matches

--- Google Worldcup Tourment.py
+++ Google Worldcup Tourment.py
@@ -3,35 +3,33 @@
 已知有个表格知道16强两两之间胜负的概率，以及一个16强对阵表。
 求输入任意一个球队，返回这个球队最终夺冠概率。
 """
+import collections
 import unittest
 from typing import List
 
 class Solution:
     def winProb(self, match: List[int], table: List[List[float]]) -> List[float]:
-        round = [{team: 1.0} for team in match] # initial round, every team survives with proba = 1.0
-        while len(round) > 1:
+        queue = collections.deque([{team: 1.0} for team in match]) # initial round, every team survives with proba = 1.0
+        while len(queue) > 1:
             # each round, merge two groups using cartesian product
             # a team survives this round with probability of it survives previous round multiplies
             # the marginal probability that it wins any team in the other groups that survived previous round
             # p(team|r) = p(team|r-1) * sum(p(team|other team) * p(other team|r-1), other team in the neighbor group)
-            next_round = []
-            for i in range(0, len(round), 2):
-                group1 = round[i]
-                group2 = round[i+1]
-                new_group = {}
-                for a in group1:
-                    p = 0.0
-                    for b in group2:
-                        p += table[a][b] * group2[b]
-                    new_group[a] = group1[a] * p
+            group1 = queue.popleft()
+            group2 = queue.popleft()
+            new_group = {}
+            for a in group1:
+                p = 0.0
                 for b in group2:
-                    p = 0.0
-                    for a in group1:
-                        p += table[b][a] * group1[a]
-                    new_group[b] = group2[b] * p
-                next_round.append(new_group)
-            round = next_round
-        return [round[0][i] for i in match]
+                    p += table[a][b] * group2[b]
+                new_group[a] = group1[a] * p
+            for b in group2:
+                p = 0.0
+                for a in group1:
+                    p += table[b][a] * group1[a]
+                new_group[b] = group2[b] * p
+            queue.append(new_group)
+        return [queue[0][i] for i in range(len(match))]
 
 
 class TestSolution(unittest.TestCase):
